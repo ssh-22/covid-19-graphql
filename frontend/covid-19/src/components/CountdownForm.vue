26b9f8745bc32aba76bdd67b14f2b06be7e0efc3
ssh-22 <r.march.13_liberty0@yahoo.ne.jp>
2020-05-23 23:17:19 +0900

    add countdown form

--- frontend/covid-19/src/components/CountdownForm.vue
+++ frontend/covid-19/src/components/CountdownForm.vue
@@ -1,7 +1,65 @@
-<template> </template>
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field v-model="target" label="何を"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-menu
+            v-model="isOpen"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="computedDateFormatted"
+                label="いつまでに"
+                prepend-icon="event"
+                readonly
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="targetDate"
+              @input="isOpen = false"
+              color="light-blue accent-2"
+              locale="ja"
+            ></v-date-picker>
+            <v-icon></v-icon>
+          </v-menu>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "CountdownForm",
+  data: (vm: any) => ({
+    target: "緊急事態宣言解除",
+    targetDate: new Date().toISOString().substr(0, 10),
+    isOpen: false
+  }),
+  computed: {
+    computedDateFormatted(): void {
+      return this.formatDate(this.targetDate);
+    }
+  },
+  methods: {
+    formatDate(date: string): any {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("-");
+      return `${year}/${month}/${day}`;
+    }
+  }
+});
 </script>
 
 <style></style>
--- frontend/covid-19/src/components/HelloWorld.vue
+++ frontend/covid-19/src/components/HelloWorld.vue
@@ -2,32 +2,24 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
         </p>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
 
         <v-row justify="center">
           <a
@@ -36,16 +28,12 @@
             :href="next.href"
             class="subheading mx-3"
             target="_blank"
-          >
-            {{ next.text }}
-          </a>
+          >{{ next.text }}</a>
         </v-row>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
         <v-row justify="center">
           <a
@@ -54,16 +42,12 @@
             :href="link.href"
             class="subheading mx-3"
             target="_blank"
-          >
-            {{ link.text }}
-          </a>
+          >{{ link.text }}</a>
         </v-row>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
         <v-row justify="center">
           <a
@@ -72,9 +56,7 @@
             :href="eco.href"
             class="subheading mx-3"
             target="_blank"
-          >
-            {{ eco.text }}
-          </a>
+          >{{ eco.text }}</a>
         </v-row>
       </v-col>
     </v-row>
